d231b0e0215b6500452fd35f4e3ba94ebf3dd266
narvydasburinsk <narvydas.burinsk@gmail.com>
2026-01-31 17:39:51 +0100

    Implement layout with title and introduction text
    Added a column layout with title and introduction text.

--- Screens/App.py
+++ Screens/App.py
@@ -3,6 +3,13 @@ from Imports.UI_visible import *
 from Imports.Utils import *
 
 def AppStart():
+    Column (
+        Row(
+            Text("Title")
+        ),
+        Row(
+            Text("Hi everybody this is my web cv, i made this entirely out of python. I know it may seem dumb. But i like it. I hope you to.")
+        )
+    )
     ui.label("App is starting...")
     ui.button("Click me", on_click=lambda: ui.notify("Hello!"))
-    ui.run()
