e256286396d3cff6301a3509e31ac7beca5d1e5f
narvydasburinsk <narvydas.burinsk@gmail.com>
2026-01-31 17:33:24 +0100

    Remove NiceGUI UI elements and start App

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-# Install first: pip install nicegui
-
 from nicegui import ui
 from Screens.App import *
 
 
-# Title
-ui.label('Hello! Welcome to NiceGUI!')
-
-# Button
-def clicked():
-    ui.notify('You clicked the button!')
-
-ui.button('Click Me', on_click=clicked)
-
-# Input box
-def submit_name():
-    ui.notify(f'Hello, {name_input.value}!')
-
-name_input = ui.input('Your Name')
-ui.button('Say Hello', on_click=submit_name)
-
-# Run app
-ui.run(title='My NiceGUI App', port=8080)
+AppStart()
